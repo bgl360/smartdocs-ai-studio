c6911e97677f57a4d7b2e8276a3b6fa0a35189f4
Li Zhao <lzhao@bglcorp.com.au>
2025-06-10 13:33:50 +1000

    BS-10188 remove entityId, add thirdpartyuserId,application.yml

--- assistant-backend-demo/java/src/main/java/com/bgl/ai/chatbot/controller/AiAssistantHeaderHelper.java
+++ assistant-backend-demo/java/src/main/java/com/bgl/ai/chatbot/controller/AiAssistantHeaderHelper.java
@@ -1,10 +1,15 @@
 package com.bgl.ai.chatbot.controller;
 
 public interface AiAssistantHeaderHelper {
+    //API Key and Third Party User ID headers are required for the request.
+    String API_KEY = "X-API-KEY";
+    String THIRD_PARTY_USER_ID = "X-ThirdParty-User-Id";
+
+    // Headers used for AI Assistant function calls
+    // Can define your own headers as per your requirement. Please start with "app-ai-assistant-function-call-"
     String USERNAME = "app-ai-assistant-function-call-user-name";
     String FIRM_ID = "app-ai-assistant-function-call-firm-id";
     String SOURCE = "app-ai-assistant-function-call-source";
-    String SOURCE_NAME= "sourceName";
-    String ENTITY_ID = "app-ai-assistant-function-call-entity-id";
-    String API_KEY = "x-api-key";
+    String SOURCE_NAME= "sourceName"; //replace with actual source name if needed, for example "sf360"
+
 }
